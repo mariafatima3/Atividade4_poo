ddd0b75ca8abb90d0cff3b52cfe177127d5645e9
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-10-10 16:45:28 +0000

    quest_37

--- quest_37/main.py
+++ quest_37/main.py
@@ -1,6 +1,5 @@
 # 37) Defina a classe Quadra com construtor que recebe largura e comprimento da quadra. Crie uma instância com dimensões 28x15 metros.
 
-
 class Quadra:
   def __init__(self,largura, comprimento):
         self.largura = largura
