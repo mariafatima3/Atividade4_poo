fe57debf98c0df70784143d9a35bd359cf654032
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-10-10 19:34:34 +0000

    quest_44

---
+++ quest_44/main.py
@@ -0,0 +1,16 @@
+# 44) Defina a classe Estatistica com construtor que recebe nome do jogador obrigatório e estatísticas opcionais via **kwargs (pontos, assistências, rebotes).
+
+class Estatistica:
+    def __init__(self, nome, **kwargs):
+        self.nome = nome
+        self.pontos = kwargs.get("pontos", 0)
+        self.assistencias = kwargs.get("assistencias", 0)
+        self.rebotes = kwargs.get("rebotes", 0)
+
+    def __str__(self):
+        return (f"Nome: {self.nome}\n"
+                f"Pontos: {self.pontos}\n" 
+                f"Assistências: {self.assistencias}\n"
+                f"Rebotes: {self.rebotes}")
+estatistica = Estatistica("João", pontos = 7, assistencias=4, rebotes = 3)
+print(estatistica)
